951d3634da83e6942378f866037b9cfc05aa018a
Jérôme Mahuet <jerome.mahuet@gmail.com>
2015-07-06 11:51:21 +0200

    refactoring

---
+++ src/Types.hs
@@ -0,0 +1,30 @@
+module Types where
+
+import Data.Semigroup
+
+
+data Point2D = Point2D { px :: Int
+                       , py :: Int
+                       } deriving (Show, Eq)
+
+instance Semigroup Point2D where
+  (Point2D a a') <> (Point2D b b') = Point2D (a+b) (a'+b')
+
+data Direction = UpD | DownD | LeftD | RightD
+  deriving (Show)
+
+instance Semigroup Direction where
+  DownD  <> UpD    = DownD
+  _      <> UpD    = UpD
+  UpD    <> DownD  = UpD
+  _      <> DownD  = DownD
+  RightD <> LeftD  = RightD
+  _      <> LeftD  = LeftD
+  LeftD  <> RightD = LeftD
+  _      <> RightD = RightD
+
+data State = State { snake     :: [Point2D]
+                   , candy     :: Point2D
+                   , direction :: Direction
+                   , finished  :: Bool
+                   } deriving (Show)
